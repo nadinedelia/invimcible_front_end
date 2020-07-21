6fdd4aa5a33244e446fec2e5206149355789e2e0
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-21 12:04:43 +0100

    Sprite now changing image but still not moving around the screen

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,21 +1,28 @@
 import React from "react"
 import Actor from "../actor"
-import useKeyPress from "../..hooks/use-key-press"
-import useWalk from "../..hooks/use-walk"
+import useKeyPress from "../../hooks/use-key-press"
+import useWalk from "../../hooks/use-walk"
 
 export default function Player({ skin }) {
-  const {dir, step, walk} = useWalk(4)
+  const {dir, step, walk, position} = useWalk(3)
   const data = {
     h: 32,
     w:32,
   };
 
-
-
   useKeyPress((e) => {
     walk(e)
+
     e.preventDefault()
   })
 
-  return <Actor sprite={`/sprites/skins/${skin}.png`} data={data} step={step} dir={dir} />;
+  return (
+    <Actor 
+      sprite={`/sprites/skins/${skin}.png`} 
+      data={data} 
+      step={step} 
+      dir={dir} 
+      position={position}
+    />
+  );
 }
