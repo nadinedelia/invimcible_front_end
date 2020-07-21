c50cd399f7e20aaa2095d41feae72bc66820337b
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-21 10:31:33 +0100

    add component directory and add sprite

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Sprite from "./components/sprite"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="zone-container">
+      <Sprite image={"/sprites/skins/vimBishop1.png"} data = {{
+        x: 0,
+        y: 0,
+        w: 32,
+        h: 32,
+      }}
+      />
     </div>
   );
 }
-
-export default App;
