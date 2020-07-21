9a48d7d08a7c3df7a2de08e469855df0dbd472cc
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-21 15:46:08 +0100

    add walk hook/function

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import Actor from "../actor";
 import useKeyPress from "../../hooks/use-key-press";
-export default function Player({ skin }) {
+import useWalk from "../../hooks/use-walk"; 
+
+export default function Player({ skin }) { 
+  const { dir, step, walk } = useWalk(3)
   const data = {
     h: 32,
     w: 32,
   };
-  const directions = {
-    down: 0,
-    left: 1,
-    right: 2,
-    up: 3,
-  };
+
   useKeyPress((e) => {
-    const dir = e.key.replace("Arrow", "").toLowerCase()
-    if(directions.hasOwnProperty(dir))
-    console.dir(dir)
+    const dir = e.key.replace("Key", "").toLowerCase()
+    walk(dir)
     e.preventDefault();
   })
   return <Actor sprite={`/sprites/skins/${skin}.png`} data={data} />;
