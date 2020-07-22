13d29123411d2970e707c0237277c2c5be375bff
Rachael <rachaelewins@hotmail.com>
2020-07-22 12:51:41 +0100

    absolute map position

--- src/components/play/index.jsx
+++ src/components/play/index.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import Player from "../player"
+import Game from "../Game.js"
 
 export default function Play() {
 return (
 <div className="zone-container">
+  <Game />
   <Player skin="m2" />
 </div>
 )
