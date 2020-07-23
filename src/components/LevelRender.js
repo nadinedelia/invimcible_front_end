f65b878151b14e3d0f06ab5d21744c39056433ec
Sophie <sophiebrown438@gmail.com>
2020-07-23 11:41:02 +0100

    Maptile function render Vim path

--- src/components/LevelRender.js
+++ src/components/LevelRender.js
@@ -4,36 +4,70 @@ import TileSprite from "./TileRender.js";
 
 // processing a tile
 function MapTile(props) {
-  // switch (props.value) {
-  //   case "B":
-  //     var tile = {
-  //       x: 0 * 32,
-  //       y: 0 * 32,
-  //     };
-  //     break;
-  //   case "F1":
-  //     var tile = {
-  //       x: 1 * 32,
-  //       y: 1 * 32,
-  //     };
-  //     break;
-  //   case "I3":
-  //     var tile = {
-  //       x: 0 * 32,
-  //       y: 2 * 32,
-  //     };
-  //     break;
-  //   case "P3":
-  //     var tile = {
-  //       x: 2 * 32,
-  //       y: 2 * 32,
-  //     };
-  //     break;
-  // }
-  var tile = {
-    x: 2 * 32,
-    y: 2 * 32,
+  switch (props.value) {
+    case "B":
+      var tile = {
+        x: 0 * 32,
+        y: 0 * 32,
+      };
+      break;
+    case "F1":
+      var tile = {
+        x: 1 * 32,
+        y: 1 * 32,
+      };
+      break;
+    case "I1" :
+      var tile = {
+        x: 0 * 32,
+        y: 2 * 32,
+      };
+      break;
+    case "I2" :
+      var tile = {
+        x: 0 * 32,
+        y: 2 * 32,
+      };
+      break;
+    case "I3" :
+      var tile = {
+        x: 0 * 32,
+        y: 2 * 32,
+      };
+      break;
+    case ("P1"):
+    var tile = {
+      x: 2 * 32,
+      y: 2 * 32,
+    };
+    break;
+    case ("P3"):
+      var tile = {
+        x: 2 * 32,
+        y: 2 * 32,
+      };
+      break;
+    case "P2":
+      var tile = {
+        x: 2 * 32,
+        y: 2 * 32,
+      };
+      break;
+    case "VS":
+    var tile = {
+      x: 4 * 32,
+      y: 4 * 32,
+    };
+    break;
+    case ("E"):
+    var tile = {
+      x: 3 * 32,
+      y: 0 * 32,
+    };
+    break;
   };
+        console.log(tile)
+
   return (
     <div class="tile">
       {" "}
