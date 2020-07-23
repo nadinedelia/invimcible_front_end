1caabf28539ea08fee82be9f98c01acbbecf3304
nadinedelia <nadine.delia@yahoo.com>
2020-07-23 16:17:44 +0100

    logo over game screen

--- src/components/play/index.jsx
+++ src/components/play/index.jsx
@@ -4,20 +4,25 @@ import Game from "../Game.js";
 
 export default function Play() {
   return (
-    <div className="game-container">
-      <div className="support-container"></div>
+    <div className="super-container">
+      <center>
+        <img src={"images/logo.png"} className="logo2" />
+        <div className="game-container">
+          <div className="support-container"></div>
 
-      <div className="container-right">
-        <div className="zone-container">
-          <Game />
-          <Player skin="m2" />
-          <br></br>
-          <br></br>
-          <div className="speech-container">
-            <p> PLEASE PRINT SMTH </p>
+          <div className="container-right">
+            <div className="zone-container">
+              <Game />
+              <Player skin="m2" />
+              <br></br>
+              <br></br>
+              <div className="speech-container">
+                <p> PLEASE PRINT SMTH </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </center>
     </div>
   );
 }
