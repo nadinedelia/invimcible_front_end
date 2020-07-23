f7d95c638611d1f1edf8e8eb7d26d26267b3efb9
Tristan Langford <tristanlangford@hotmail.com>
2020-07-23 11:23:54 +0100

    added speech block and support block

--- src/components/play/index.jsx
+++ src/components/play/index.jsx
@@ -6,9 +6,17 @@ import Game from "../Game.js";
 
 export default function Play() {
   return (
+    <div className="game-container">
+      <div className="support-container"></div>
+
     <div className="zone-container">
       <Game />
       <Player skin="m2" />
+      <br></br>
+      <br></br>
+      <div className="speech-container">
+      </div>
+    </div>
     </div>
   );
 }
