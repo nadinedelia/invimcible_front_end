e721f5a03da942264e604f0bc52788b0feabfeab
nadinedelia <nadine.delia@yahoo.com>
2020-07-23 15:13:24 +0100

    switch buttons

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -7,11 +7,11 @@ export default function Home() {
       <center>
         <div class="background-container">
           <img src={image} alt="HomeScreen" id="vimGame"></img>
-          <a href="/play">
-            <button class="start-btn">START GAME</button>
-          </a>
           <a href="./about">
             <button class="about-btn">ABOUT</button>
+          </a>
+          <a href="/play">
+            <button class="start-btn">START GAME</button>
           </a>
         </div>
       </center>
