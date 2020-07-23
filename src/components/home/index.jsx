a409cf78a516aa84ed4d8b12bb27d5df33edfa8b
nadinedelia <nadine.delia@yahoo.com>
2020-07-23 15:49:59 +0100

    center start page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,18 +3,16 @@ import image from "../../images/home_screen_still.jpg";
 
 export default function Home() {
   return (
-    <div class="homescreencontainer">
-      <center>
-        <div class="background-container">
-          <img src={image} alt="HomeScreen" id="vimGame"></img>
-          <a href="./about">
-            <button class="about-btn">ABOUT</button>
-          </a>
-          <a href="/play">
-            <button class="start-btn">START GAME</button>
-          </a>
-        </div>
-      </center>
-    </div>
+    <center>
+      <div class="homescreencontainer">
+        <img src={image} alt="HomeScreen" id="vimGame"></img>
+        <a href="./about">
+          <button class="about-btn">ABOUT</button>
+        </a>
+        <a href="/play">
+          <button class="start-btn">START GAME</button>
+        </a>
+      </div>
+    </center>
   );
 }
