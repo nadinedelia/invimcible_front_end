5c823258b6857caed6931d751d867d996336cb86
nadinedelia <nadine.delia@yahoo.com>
2020-07-23 15:07:32 +0100

    start page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,26 +1,20 @@
-import React from "react"
-import image from "../../images/home_screen_still.jpg"
+import React from "react";
+import image from "../../images/home_screen_still.jpg";
 
 export default function Home() {
-  const style = {
-    height: '700',
-    width: '100%',
-  };
-  return(
+  return (
     <div class="homescreencontainer">
       <center>
-      <div class="background-container">
-        <img src={image} 
-        alt="HomeScreen"
-        id="vimGame"
-        style={ style }
-        ></img>
-        <a href='/play'><button class="start-btn">START GAME</button></a>
-        <a href='./about'><button class="about-btn">ABOUT</button></a>
-      </div>
+        <div class="background-container">
+          <img src={image} alt="HomeScreen" id="vimGame"></img>
+          <a href="/play">
+            <button class="start-btn">START GAME</button>
+          </a>
+          <a href="./about">
+            <button class="about-btn">ABOUT</button>
+          </a>
+        </div>
       </center>
     </div>
-  )
+  );
 }
-
-
