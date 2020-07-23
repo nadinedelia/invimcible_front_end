cd1c01f66c865bd904f4600139da444631ca40b5
nadinedelia <nadine.delia@yahoo.com>
2020-07-23 15:25:45 +0100

    github names

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,42 +1,45 @@
-import React from "react"
+import React from "react";
 
-export default function About () {
+export default function About() {
   return (
     <div className="about">
-    <center>
-      <div class="background-container">
-        <p> Hi, we are the creators of The adventures of Vim. </p>
-        <p> We are six budding software developers who joined Makers Academy in May 2020 and built The Adventures of Vim as our final project. </p>
-        <p> Click on our names to see our other work. </p>
+      <center>
+        <div class="background-container">
+          <p> Hi, we are the creators of The adventures of Vim. </p>
+          <p>
+            {" "}
+            We are six budding software developers who joined Makers Academy in
+            May 2020 and built The Adventures of Vim as our final project.{" "}
+          </p>
+          <p> Click on our names to see our other work. </p>
 
-        <div class="about-images-container">
+          <div class="githubs">
+            <a href="https://github.com/sofyloafy">
+              <p>Sophie Brown</p>
+            </a>
 
-          <div class="grid-item">
-            <a href='https://github.com/sofyloafy'><p>Sophie Brown</p></a>
-          </div>
+            <a href="https://github.com/Sumner1185">
+              <p>Al Sumner</p>
+            </a>
 
-          <div class="grid-item">
-            <a href='https://github.com/Sumner1185'><p>Al Sumner</p></a>
-          </div>
+            <a href="https://github.com/tristanlangford">
+              <p>Tristan Langford</p>
+            </a>
 
-          <div class="grid-item">
-            <a href='https://github.com/tristanlangford'><p>Tristan Langford</p></a>
-          </div>
+            <a href="https://github.com/raerachael">
+              <p>Rachael Ewins</p>
+            </a>
 
-          <div class="grid-item">
-            <a href='https://github.com/raerachael'><p>Rachael Ewins</p></a>
-          </div>
+            <a href="https://github.com/nadinedelia">
+              <p>Nadine Loepfe</p>
+            </a>
 
-          <div class="grid-item">
-            <a href='https://github.com/nadinedelia'><p>Nadine Loepfe</p></a>
-          </div>
-
-          <div class="grid-item">
-            <a href='https://github.com/katie-mcdonagh'><p>Katie McDonagh</p></a>
+            <a href="https://github.com/katie-mcdonagh">
+              <p>Katie McDonagh</p>
+            </a>
           </div>
         </div>
-      </div>
-    </center>
+      </center>
     </div>
   );
 }
