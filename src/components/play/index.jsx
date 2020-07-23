d361b88507d17456047aa382de5b63c38ea40a13
nadinedelia <nadine.delia@yahoo.com>
2020-07-23 11:44:12 +0100

    grid layout

--- src/components/play/index.jsx
+++ src/components/play/index.jsx
@@ -1,22 +1,21 @@
-
 import React from "react";
 import Player from "../player";
 import Game from "../Game.js";
-
 
 export default function Play() {
   return (
     <div className="game-container">
       <div className="support-container"></div>
 
-    <div className="zone-container">
-      <Game />
-      <Player skin="m2" />
-      <br></br>
-      <br></br>
-      <div className="speech-container">
+      <div className="container-right">
+        <div className="zone-container">
+          <Game />
+          <Player skin="m2" />
+          <br></br>
+          <br></br>
+          <div className="speech-container"></div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
