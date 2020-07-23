5add70fb725622c801389fea649257a9831063f0
Rachael <rachaelewins@hotmail.com>
2020-07-23 11:16:36 +0100

    /help displays vim commands

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react"
 import Home from "./components/home/index.jsx"
 import About from "./components/about/index.jsx"
 import Play from "./components/play"
+import HelpScreen from "./components/helpScreen/helpScreen.jsx"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Player from "./components/player"
 
@@ -15,6 +16,7 @@ export default function App() {
       <Route path="/" exact component={Home}/>
       <Route path="/play" exact component={Play}/>
       <Route path="/about" exact component={About}/>
+      <Route path="/help" exact component={HelpScreen}/>
     </Router>
   );
 }
