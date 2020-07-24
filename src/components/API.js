523a025f8988aeb7b2b94173e6acef2f3a854a3e
Rachael <rachaelewins@hotmail.com>
2020-07-24 11:40:34 +0100

    api connection outside render

---
+++ src/components/API.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class API extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      tiles: [],
+    };
+  };
+
+  makeRequest() {
+    // API connection code
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result, "hi")
+          this.state = {
+            isLoaded: true,
+            tiles: [...result.level1],
+          };
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  TileMatrix() {
+    if (this.state.isLoaded === true) {
+      return this.state.tiles
+    } else { return "not yet"}
+  }
+}
+
+export default API;
