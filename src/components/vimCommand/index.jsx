96004b45ca5579a1cc69d47ff8a990fb4bda47e5
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-27 14:28:54 +0100

    save state of keypress

--- src/components/vimCommand/index.jsx
+++ src/components/vimCommand/index.jsx
@@ -1,16 +1,42 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import "./vimCommand.css"
+import toggleShowHelp from "../play/index.jsx"
 
 export default class VimCommand extends React.Component {
+  constructor () {
+  super();
+  this.state = {
+
+    prefix: ':'
+
+  }
+
+    // getCommand() {
+    //   document.getElementsByClassName("vimBox");
+
+    // }
+
+
+
+  }
+  handleChange(e) {
+    console.log(e.target.value)
+    // on hold until redux is up and running
+    // if (e.target.value === ':h'){
+    //   toggleShowHelp()
+    // } 
+    };
 
     render () {
       return <div class= "vimBox">
           <input
             type="text"
-            value=":"
+            defaultValue={this.state.prefix}
+            value = {this.state.value}
+            onChange = {this.handleChange}
          />
       </div>
-
-}
+    
+  }
 }
