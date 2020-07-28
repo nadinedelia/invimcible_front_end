0af14bfde80105c1a35e8b57e5c9e09871f960ed
Rachael <rachaelewins@hotmail.com>
2020-07-28 14:38:09 +0100

    spiked loads level 2

--- src/components/API.js
+++ src/components/API.js
@@ -3,25 +3,45 @@ import store from "../config/store";
 
 class API extends React.Component {
 
-  makeRequest() {
+  makeRequest(number = 1) {
     // API connection code
-    fetch(
-      "https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/"
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          console.log(result.level1Data)
-          store.dispatch({
-            type: "ADD_TILES",
-            payload: {
-              loaded: true,
-              tiles: [...result.level1Data.mapArray],
-              startingPoint: result.level1Data.startingPoint
-            },
-          })
-        }
-      );
+    if (number === 1) {
+      fetch(
+        "https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/"
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            console.log(result.level1Data)
+            store.dispatch({
+              type: "ADD_TILES",
+              payload: {
+                loaded: true,
+                tiles: [...result.level1Data.mapArray],
+                startingPoint: result.level1Data.startingPoint
+              },
+            })
+          }
+        );
+    } else if (number === 2) {
+      fetch(
+        "https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/"
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            console.log(result.level1Data)
+            store.dispatch({
+              type: "ADD_TILES",
+              payload: {
+                loaded: true,
+                tiles: [...result.level2Data.mapArray],
+                startingPoint: result.level2Data.startingPoint
+              },
+            })
+          }
+        );
+    }
   }
 }
 
