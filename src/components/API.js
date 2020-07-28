9d1dea6ee491cd1639bbc890cf95a8b3859c2da2
Rachael <rachaelewins@hotmail.com>
2020-07-28 15:01:44 +0100

    api set for multiple pages

--- src/components/API.js
+++ src/components/API.js
@@ -5,43 +5,23 @@ class API extends React.Component {
 
   makeRequest(number = 1) {
     // API connection code
-    if (number === 1) {
-      fetch(
-        "https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/"
-      )
-        .then((res) => res.json())
-        .then(
-          (result) => {
-            console.log(result.level1Data)
-            store.dispatch({
-              type: "ADD_TILES",
-              payload: {
-                loaded: true,
-                tiles: [...result.level1Data.mapArray],
-                startingPoint: result.level1Data.startingPoint
-              },
-            })
-          }
-        );
-    } else if (number === 2) {
-      fetch(
-        "https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/"
-      )
-        .then((res) => res.json())
-        .then(
-          (result) => {
-            console.log(result.level1Data)
-            store.dispatch({
-              type: "ADD_TILES",
-              payload: {
-                loaded: true,
-                tiles: [...result.level2Data.mapArray],
-                startingPoint: result.level2Data.startingPoint
-              },
-            })
-          }
-        );
-    }
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/${number}`
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          store.dispatch({
+            type: "ADD_TILES",
+            payload: {
+              loaded: true,
+              tiles: [...result.mapArray],
+              startingPoint: result.startingPoint
+            },
+          })
+        }
+      );
   }
 }
 
