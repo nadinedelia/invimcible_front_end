09b5c89aaa68c3515ba54ad83b9069a23c9a9f2a
nadinedelia <nadine.delia@yahoo.com>
2020-07-28 12:37:54 +0100

    speech box gone

--- src/worldRoot.js
+++ src/worldRoot.js
@@ -1,44 +1,43 @@
 import React, { Component, useState } from "react";
 import World from "./features/world";
 
-import VimCommand from "./components/vimCommands"
-import Help from './components/helpScreen/help'
+import VimCommand from "./components/vimCommands";
+import Help from "./components/helpScreen/help";
 import store from "./config/store";
-import  { Redirect } from 'react-router-dom'
+import { Redirect } from "react-router-dom";
 
 function WorldRoot() {
-
   const [quit, setQuit] = useState(false);
   const [displayHelp, setShowHelp] = useState(false);
   const [displayVimCommand, setShowVimCommand] = useState(false);
 
-   function toggleShowVimCommand() {
-    setShowVimCommand(true)
-   }
+  function toggleShowVimCommand() {
+    setShowVimCommand(true);
+  }
 
-   function handleKeyDown(e) {
-     switch (e.key) {
+  function handleKeyDown(e) {
+    switch (e.key) {
       case ":":
-        return toggleShowVimCommand()
-      case "Backspace": 
-      console.log(store.getState().vimCommand)
-        if(store.getState().vimCommand === ":") {
-          setShowHelp(false)
-          return setShowVimCommand(false)
+        return toggleShowVimCommand();
+      case "Backspace":
+        console.log(store.getState().vimCommand);
+        if (store.getState().vimCommand === ":") {
+          setShowHelp(false);
+          return setShowVimCommand(false);
         }
       case "Enter":
         if (displayVimCommand) {
           if (store.getState().vimCommand === ":h") {
-            return setShowHelp(true)
+            return setShowHelp(true);
           } else if (store.getState().vimCommand === ":q") {
-              changeQuit()
+            changeQuit();
           }
         }
-      }
     }
+  }
 
   function changeQuit() {
-    setQuit(true)
+    setQuit(true);
   }
 
   window.addEventListener("keydown", (e) => {
@@ -58,14 +57,11 @@ function WorldRoot() {
 
           <div className="container-right">
             <div className="zone-container">
-              { displayHelp ? <Help /> : null}
+              {displayHelp ? <Help /> : null}
               <World />
 
-              { displayVimCommand ? <VimCommand /> : null }
-              { quit ? <Redirect to='/' /> : null }
-              <div className="speech-container">
-                <p> PLEASE PRINT SMTH </p>
-              </div>
+              {displayVimCommand ? <VimCommand /> : null}
+              {quit ? <Redirect to="/" /> : null}
             </div>
           </div>
         </div>
