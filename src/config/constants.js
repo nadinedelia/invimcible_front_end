7ffeb65bfd70ae03a724951e383e71d6f6940542
Sophie <sophiebrown438@gmail.com>
2020-07-30 16:24:13 +0100

    Amend Person name in interaction

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,9 +3,10 @@ export const MAP_HEIGHT = 13 * SPRITE_SIZE;
 export const MAP_WIDTH = 23 * SPRITE_SIZE;
 const Names = { name1: "Katie", name2: "Nads", name3: "Sophie" };
 export const SCRIPT_1 = {
-  P1: `\n${Names.name1.bold()}: Hi, Vim! I haven’t seen your bike, I’m afraid. Maybe try down there!`,
+  P1: `\n${Names.name1}: Hi, Vim! I haven’t seen your bike, I’m afraid. Maybe try down there!`,
   P2:
-    "\nPerson 2: Vim, my guy! I’m surprised to see you walking… Oh no, I haven’t seen your bike tied up anywhere. Good luck searching!",
+    `\n${Names.name2}: Vim, my guy! I’m surprised to see you walking… Oh no, I haven’t seen your bike tied up anywhere. Good luck searching!`,
   P3:
-    "\nPerson 3: Hello Vim. I did see someone else with a bike that looked similar to yours a few minutes ago. They cycled that way! Bye Vim!",
+    `\n${Names.name3}: Hello Vim. I did see someone else with a bike that looked similar to yours a few minutes ago. They cycled that way! Bye Vim!`,
 };
+
