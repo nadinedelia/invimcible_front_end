8229ccc54b10c2ab023ea013f22f207a89fa8ee3
nadinedelia <nadine.delia@yahoo.com>
2020-07-30 14:05:20 +0100

    new tiles

--- src/components/API.js
+++ src/components/API.js
@@ -3,32 +3,32 @@ import store from "../config/store";
 import { SPRITE_SIZE } from "../config/constants";
 
 class API extends React.Component {
-
   makeRequest(number = 1) {
     // API connection code
     fetch(
-      `https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/${number}`
+      `https://cors-anywhere.herokuapp.com/https://vim-back-end.herokuapp.com/2`
     )
       .then((res) => res.json())
-      .then(
-        (result) => {
-          store.dispatch({
-            type: "ADD_TILES",
-            payload: {
-              loaded: true,
-              tiles: [...result.mapArray],
-            },
-          })
-          store.dispatch({
-            type: "VIM_START",
-            payload: {
-              canMove: true,
-              position: [result.startingPoint.x * SPRITE_SIZE, result.startingPoint.y * SPRITE_SIZE],
-              walkIndex: 0
-            }
-          })
-        }
-      );
+      .then((result) => {
+        store.dispatch({
+          type: "ADD_TILES",
+          payload: {
+            loaded: true,
+            tiles: [...result.mapArray],
+          },
+        });
+        store.dispatch({
+          type: "VIM_START",
+          payload: {
+            canMove: true,
+            position: [
+              result.startingPoint.x * SPRITE_SIZE,
+              result.startingPoint.y * SPRITE_SIZE,
+            ],
+            walkIndex: 0,
+          },
+        });
+      });
   }
 }
 
