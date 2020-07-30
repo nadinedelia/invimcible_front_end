d28e4e8aa5ac951dee0f271d028edb784b002ca8
Tristan Langford <tristanlangford@hotmail.com>
2020-07-30 11:45:01 +0100

    removed comments

--- src/components/ApiConnection.test.js
+++ src/components/ApiConnection.test.js
@@ -1,15 +1,8 @@
 import React from "react";
-
 import World from "../features/world";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-
 import API from "./API.js";
 import store from "../config/store";
 import {shallow} from 'enzyme';
-
-// const supertest = require('supertest');
-// const http = require('http')
 
 
 describe("Level 1 connection", () => {
@@ -39,7 +32,6 @@ describe("Level 1 connection", () => {
     setTimeout(function (){
 
       expect(store.getState().map).toBe(fakeResponse);
-        // Something you want delayed.
       
       }, 100)
   });
@@ -62,26 +54,10 @@ describe("Level 1 connection", () => {
       return Promise.resolve(fetchResponse);
     });
 
-    // get request for /play
-
-    // return supertest(app)
-    // .get("/play")
-    // .then(response => {
-    //   expect(response.statusCode).toBe(404);
-    // });
-
     const container = shallow(<World />)
 
     let containerStyle = container.find('.tile B').get(0).style;
-expect(containerStyle).toHaveProperty('opacity', '1');
+    expect(containerStyle).toHaveProperty('opacity', '1');
 
-//     const elem = container.find(".tile B");
-// expect(getComputedStyle(elem.getDOMNode()).getPropertyValue('opacity')).toBe('0.4');
-
-    console.log(container.find(".tile B"))
-
-    //expect(container.find('.tile B').prop('style')).toHaveProperty('', '100%');
-
-    //expect documnet.getElementByClass(tile B) match the styles
   })
 });
