d65a853196dfeb593255dbf2e0cfa65d5cd8f26d
Tristan Langford <tristanlangford@hotmail.com>
2020-07-30 11:34:56 +0100

    change help based on level

--- src/components/helpScreen/help.jsx
+++ src/components/helpScreen/help.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import store from "../../config/store"
 // import HelpScreen from "./helpScreen.jsx"
 import HelpScreenLevel1 from "./helpScreenLevel1.jsx"
-// import HelpScreenLevel2 from "./helpScreenLevel2.jsx"
+import HelpScreenLevel2 from "./helpScreenLevel2.jsx"
 // import { render } from "@testing-library/react"
 
 export default class Help extends React.Component {
+  getLevel() {
+    return store.getState().map.level
+  }
     render () {
+      if(this.getLevel() === 1) {
       return <HelpScreenLevel1 />
+      } else {
+        return <HelpScreenLevel2 />
+      }
 }
 }
